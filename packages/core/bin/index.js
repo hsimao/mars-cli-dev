2b61fe662fdc8d73abf7dac91a8a328c81d93d5d
hsimao <e087754958@gmail.com>
2021-08-16 20:19:43 +0800

    edit: 調整架構

--- packages/core/bin/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-const utils = require("@mars-cli-dev/utils");
-
-console.log("hello mars-cli-devkk");
-utils()
--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -4,5 +4,5 @@ module.exports = utils;
 
 function utils() {
     // TODO
-    console.log('utilsoqq')
+    console.log('utils')
 }
