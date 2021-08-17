d63428740f84471f392941dc1b0576d9a639f4b4
hsimao <e087754958@gmail.com>
2021-08-17 12:01:59 +0800

    feat: checkNodeVersion

---
+++ core/cli/lib/constant.js
@@ -0,0 +1,5 @@
+const LOWEST_NODE_VERSION = '14.15.0'
+
+module.exports = {
+  LOWEST_NODE_VERSION,
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,13 +2,29 @@
 
 module.exports = core
 
+const semver = require('semver')
+const colors = require('colors')
 const pkg = require('../package.json')
 const log = require('@mars-cli-dev/log')
+const { LOWEST_NODE_VERSION } = require('./constant')
 
 function core() {
-  console.log('exec core')
-  checkVersion()
-  // TODO
+  try {
+    checkVersion()
+    checkNodeVersion()
+  } catch (e) {
+    log.error(e.message)
+  }
+}
+
+function checkNodeVersion() {
+  if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+    throw new Error(
+      colors.red(
+        `mars-cli-dev 需要安裝 v${LOWEST_NODE_VERSION} 以上版本的 Node.js`
+      )
+    )
+  }
 }
 
 function checkVersion() {
