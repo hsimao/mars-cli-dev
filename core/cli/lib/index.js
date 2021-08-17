d4348da63c5bb74a18585fe016af8facd7f742da
hsimao <e087754958@gmail.com>
2021-08-17 19:42:46 +0800

    feat: updateLogLevelEnv

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -7,6 +7,7 @@ const colors = require('colors')
 const userHome = require('user-home')
 const pathExists = require('path-exists').sync
 const rootCheck = require('root-check')
+const minimist = require('minimist')
 const pkg = require('../package.json')
 const log = require('@mars-cli-dev/log')
 const { LOWEST_NODE_VERSION } = require('./constant')
@@ -17,9 +18,25 @@ function core() {
     checkNodeVersion()
     checkRoot()
     checkUserHome()
+    checkInputArgs()
+    log.verbose('debug', 'test debug log')
   } catch (e) {
     log.error(e.message)
   }
+}
+
+function checkInputArgs() {
+  const args = minimist(process.argv.slice(2))
+  updateLogLevelEnv(args)
+}
+
+function updateLogLevelEnv(args) {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose'
+  } else {
+    process.env.LOG_LEVEL = 'info'
+  }
+  log.level = process.env.LOG_LEVEL
 }
 
 function checkUserHome() {
@@ -32,7 +49,6 @@ function checkRoot() {
   // 避免使用 root, 如果使用 sudo root 權限執行, 後續可能會出現許多權限報錯, 沒辦法讀寫檔案,
   // 使用 rootCheck 將自動降級
   rootCheck()
-  console.warn('root', process.geteuid())
 }
 
 function checkNodeVersion() {
